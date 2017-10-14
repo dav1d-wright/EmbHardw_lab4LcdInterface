c37bd7b49430bc7fe4192d9d47f965db5a563dfd
David Wright <david.dw.wright@siemens.com>
2017-10-14 14:53:34 +0200

    Added RX process draft.

--- lcdDriver.vhdl
+++ lcdDriver.vhdl
@@ -1,5 +1,6 @@
 library ieee;
 use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
 entity LcdDriver is
     port
@@ -15,6 +16,7 @@ entity LcdDriver is
         BeginBurstTransfer_DI:  in std_logic;
         BurstCount_DI:          in std_logic_vector (7 downto 0);
         
+        WaitReq_SO:             out std_logic;
         ReadData_DO:		    out std_logic_vector (15 downto 0);
         
       
@@ -46,18 +48,33 @@ architecture LCD of LcdDriver is
     end component;
 
 
-    type RxState_T          is (RxStateReset, RxStateIdle, RxStateWaiting, RxStateRx);
-    type TxState_T          is (TxSTateReset, TxStateIdle, TxStateWaiting, TxStateTx);
+    -- State macine types & signals
+    type RxState_T              is (RxStateReset, RxStateIdle, RxStateWaiting, RxStateRx);
+    type TxState_T              is (TxSTateReset, TxStateIdle, TxStateWaiting, TxStateTx);
     
-    signal RxStateNext_D:   RxState_T;
-    signal TxStateNext_D:   TxState_D;
+    signal RxStateNext_D:       RxState_T;
+    signal TxStateNext_D:       TxState_D;
 
-    signal RxStatePres_D:   RxState_T;
-    signal TxStatePres_D:   TxState_D;   
+    signal RxStatePres_D:       RxState_T;
+    signal TxStatePres_D:       TxState_D;   
 
-
+    -- FIFO signals
+    signal Pop_S:               std_logic;
+    signal Push_S:              std_logic;
+    signal RxData_D:            std_logic_vector (15 downto 0);
+    signal TxData_D:            std_logic_vector (15 downto 0);
+    signal FifoFull_D:          std_logic;
+    signal FifoEmpty_D:         std_logic;
     
 begin
+    dataBuffer: FIFO generic map(256) port map(Clk_CI => Clk_CI,
+                                                Reset_BRI => Reset_BRI,
+                                                Push_SI => Push_S,
+                                                Pop_SI => Pop_S, 
+                                                Data_DI => RxData_D,
+                                                Data_DO => TxData_D
+                                                Full_SO => FifoFull_D,
+                                                Empty_SO => FifoEmpty_D);
 --------------------------------------------------------------------------------
 ---                                                                          ---
 --- Receiver state machine                                                   ---
@@ -72,11 +89,101 @@ begin
         end
     end
     
-    
     logicRx process(Clk_CI)
     begin
         
     end
+    
+    rx: process(Clk_CI, ByteEnable_DI, Write_SI)
+        variable burstCount_D:  std_logic_vector (7 downto 0);
+    begin
+        if((RxStatePres_D = RxStateRx) and (ByteEnable_DI = '1') and (Write_SI = '1'))
+            -- word transfer
+            case Address_DI is
+                when "00"
+                    -- write command
+                    if(FifoFull_D = '1')
+                        WaitReq_SO <= '1';
+                        Push_S <= '0';
+                    else
+                        WaitReq_SO <= '0';
+                        RxData_D <= WriteData_DI;
+                        Push_S <= '1';
+                        Push_S <= '0';
+                    end if;
+                        
+                when "01"
+                    -- write data7
+                    if(FifoFull_D = '1')
+                        WaitReq_SO <= '1';
+                        Push_S <= '0';
+                    else
+                        WaitReq_SO <= '0';
+                        RxData_D <= x"002C";
+                        Push_S <= '1';
+                        Push_S <= '0';
+                        RxData_D <= WriteData_DI;
+                        Push_S <= '1';
+                        Push_S <= '0';
+                    end if;
+                else
+                    RxData_D <= '0';
+                    Push_S <= '0';
+            end case
+        elsif((RxStatePres_D = RxStateRx) and (BeginBurstTransfer_DI = '1') and (Write_SI = '1'))
+            -- burst transfer
+            burstCount_D := BurstCount_DI;
+            case Address_DI is
+                when "00"
+                    -- write command
+                    while (burstCount_D > 0) loop
+                        if(Write_SI = '1')then
+                            if(FifoFull_D = '1')
+                                WaitReq_SO <= '1';
+                                Push_S <= '0';
+                                burstCount_D := burstCount_D
+                            else
+                                WaitReq_SO <= '0';
+                                RxData_D <= WriteData_DI;
+                                Push_S <= '1';
+                                Push_S <= '0';
+                                burstCount_D := burstCount_D - 1;
+                            end if;
+                        else
+                            null;
+                        end if;
+                    end loop;
+                        
+                when "01"
+                    -- write data
+                    while (burstCount_D > 0) loop
+                        if(Write_SI = '1')then
+                            if(FifoFull_D = '1')then
+                                WaitReq_SO <= '1';
+                                Push_S <= '0';
+                                burstCount_D := burstCount_D;
+                            else
+                                WaitReq_SO <= '0';
+                                RxData_D <= x"002C";
+                                Push_S <= '1';
+                                Push_S <= '0';
+                                RxData_D <= WriteData_DI;
+                                Push_S <= '1';
+                                Push_S <= '0';
+                                burstCount_D := burstCount_D - 1;
+                            end if;
+                        else
+                            null;
+                        end if;
+                    end loop;
+                else
+                    RxData_D <= '0';
+                    Push_S <= '0';
+            end case
+        else
+            null;
+        end if
+    end    
 
 --------------------------------------------------------------------------------
 ---                                                                          ---
